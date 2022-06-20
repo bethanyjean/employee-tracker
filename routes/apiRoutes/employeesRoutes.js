30dfc070c1058d1fe5de792987a7af2193bf47fd
Bethany Griffin <bethanyjeanbg@gmail.com>
2022-06-19 20:14:39 -0400

    added employee  Routes

---
+++ routes/apiRoutes/employeesRoutes.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+// Get all employees and their department affiliation
+router.get('/employees', (req, res) => {
+  const sql = `SELECT employees.*, role.title 
+                AS title 
+                FROM employees 
+                LEFT JOIN roles 
+                ON employees.role_id = roles.id, 
+                departments.name 
+                AS department_name 
+                FROM employees 
+                LEFT JOIN departments 
+                ON employees.department_id = departments.id
+                role.salary 
+                AS salary 
+                FROM employees 
+                LEFT JOIN roles 
+                ON employees.role_id = roles.id`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+
+// Create a employee
+router.post('/employee', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'role_id',
+    'manager_id'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+  const params = [
+    body.first_name,
+    body.last_name,
+    body.role_id,
+    body.manager_id
+  ];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Update a employee's role
+router.put('/employee/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'role_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE employees SET role_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.role_id, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'employee not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+
+module.exports = router;
